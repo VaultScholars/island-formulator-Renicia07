0f5b0e81f87e1509d1a3172501c3a6071773f22f
supherman <herman@supherman.io>
2026-03-11 17:00:26 -0600

    restore Renicia repository

---
+++ island_formulator/app/controllers/ingredients_controller.rb
@@ -0,0 +1,75 @@
+class IngredientsController < ApplicationController
+  before_action :require_authentication
+  before_action :set_ingredient, only: %i[ show edit update destroy ]
+
+  # GET /ingredients or /ingredients.json
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  # GET /ingredients/1 or /ingredients/1.json
+  def show
+  end
+
+  # GET /ingredients/new
+  def new
+    @ingredient = current_user.ingredients.build
+  end
+
+  # GET /ingredients/1/edit
+  def edit
+  end
+
+  # POST /ingredients or /ingredients.json
+  def create
+    @ingredient = current_user.ingredients.build(ingredient_params)
+
+    respond_to do |format|
+      if @ingredient.save
+        format.html { redirect_to @ingredient, notice: "Ingredient was successfully created." }
+        format.json { render :show, status: :created, location: @ingredient }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /ingredients/1 or /ingredients/1.json
+  def update
+    respond_to do |format|
+      if @ingredient.update(ingredient_params)
+        format.html { redirect_to @ingredient, notice: "Ingredient was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @ingredient }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /ingredients/1 or /ingredients/1.json
+  def destroy
+    @ingredient.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to ingredients_path, notice: "Ingredient was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ingredient
+      @ingredient = Ingredient.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def ingredient_params
+      params.expect(ingredient: [ :name, :category, :description, :notes ])
+      # Add tag_ids: [] to the permit list
+      # The [] means we are expecting an ARRAY of IDs (since you can check multiple boxes).
+      # Note: tag_ids: [] must be the LAST parameter in the permit() call
+      params.expect(ingredient: [ :name, :category, :description, :notes, tag_ids: [] ])
+    end
+end
---
+++ island_formulator/db/schema.rb
@@ -0,0 +1,64 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_03_06_234918) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "ingredient_tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "ingredient_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_ingredient_tags_on_ingredient_id"
+    t.index ["tag_id"], name: "index_ingredient_tags_on_tag_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.text "notes"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_ingredients_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "ingredient_tags", "ingredients"
+  add_foreign_key "ingredient_tags", "tags"
+  add_foreign_key "ingredients", "users"
+  add_foreign_key "sessions", "users"
+end
