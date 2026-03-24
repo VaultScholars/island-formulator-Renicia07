0cf2cc2518ebb847aef874883e3de69f5c4bbdfa
Renicia <reniciaclayton1@gmail.com>
2026-03-24 14:50:32 +0000

    Implemented favourite icon for recipes

--- island_formulator/app/controllers/favorites_controller.rb
+++ island_formulator/app/controllers/favorites_controller.rb
@@ -1,13 +1,14 @@
 class FavoritesController < ApplicationController
 
-  def create
+   def create
     recipe = Recipe.find(params[:recipe_id])
     current_user.favorites.create(recipe: recipe)
     redirect_to recipes_path
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(recipe_id: params[:recipe_id])
+    recipe = Recipe.find(params[:recipe_id])
+    favorite = current_user.favorites.find_by(recipe: recipe)
     favorite.destroy if favorite
     redirect_to recipes_path
   end
--- island_formulator/app/models/user.rb
+++ island_formulator/app/models/user.rb
@@ -6,7 +6,12 @@ class User < ApplicationRecord
   has_many :inventory_items, dependent: :destroy
   has_many :batches, dependent: :destroy
   has_many :favorites, dependent: :destroy
- 
+  has_many :favorite_recipes, through: :favorites, source: :recipe
+
+  def favorited?(recipe)
+    favorites.exists?(recipe: recipe)
+  end
+  
   # Add validations for better error messages
   validates :email_address, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }, confirmation: true
